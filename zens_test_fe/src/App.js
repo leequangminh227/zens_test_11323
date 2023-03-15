4996d4597a64348adee2ce7583d51381f4efc0c2
DESKTOP-JLEE3UA\leequ <leequangminh227@gmail.com>
2023-03-15 15:22:00 +0700

    update joke website 1.0

--- zens_test_fe/src/App.js
+++ zens_test_fe/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import logo from "./img/logo.png";
+import avatar from "./img/avatar.png";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -33,36 +34,44 @@ function App() {
         'A housewife, an accountant and a lawyer were asked "How much is 2+2?" The housewife replies: "Four!". The accountant says: "I think it\'s either 3 or 4. Let me run those figures through my spreadsheet one more time." The lawyer pulls the drapes, dims the lights and asks in a hushed voice, "How much do you want it to be?"',
     },
   ];
-  const [id, setId] = useState(1)
-  const [idList, setIdList] = useState([])
+  const [id, setId] = useState(1);
+  const [idList, setIdList] = useState([]);
 
   const getJoke = () => {
-    let joke = "That's all the jokes for today! Come back another day!"
-    jokeList.map((ele,index)=>{
-      if(ele.id === id){
-        joke = ele.content
+    let joke = "That's all the jokes for today! Come back another day!";
+    jokeList.map((ele, index) => {
+      if (ele.id === id) {
+        joke = ele.content;
       }
-    })
-    return joke
-  }
+    });
+    return joke;
+  };
 
   const reactJoke = () => {
-    let newJokeId = 0
-    for (let i = 0; i< jokeList.length; i++){
-      if(!idList.includes(jokeList[i].id)){
-        newJokeId = jokeList[i].id
-        setIdList([...idList, jokeList[i].id])
+    let newJokeId = 0;
+    for (let i = 0; i < jokeList.length; i++) {
+      if (!idList.includes(jokeList[i].id)) {
+        newJokeId = jokeList[i].id;
+        setIdList([...idList, jokeList[i].id]);
       }
     }
-    setId(newJokeId)
-  }
+    setId(newJokeId);
+  };
 
   return (
     <div className="app">
       <header className="appContainer">
         <div className="navbar">
-          <img className="logo" src={logo} />
-          <img className="logo" src={logo} />
+          <img className="logo" alt='logo' src={logo} />
+          <div className="avatarBlock">
+            <div className="userInfo">
+              <div className="text1">Handicrafted by</div>
+              <div className="text2">Jim HLS</div>
+            </div>
+            <div>
+              <img className="avatar" alt='avatar' src={avatar}/>
+            </div>
+          </div>
         </div>
       </header>
       <body>
@@ -78,10 +87,22 @@ function App() {
             <hr className="endJokeLine" />
             <div className="reactBlock">
               <div className="reactButton blue">
-                <button onClick={()=> {reactJoke()}}>This is Funny!</button>
+                <button
+                  onClick={() => {
+                    reactJoke();
+                  }}
+                >
+                  This is Funny!
+                </button>
               </div>
               <div className="reactButton green">
-                <button onClick={()=> {reactJoke()}}>This is not funny.</button>
+                <button
+                  onClick={() => {
+                    reactJoke();
+                  }}
+                >
+                  This is not funny.
+                </button>
               </div>
             </div>
           </div>
@@ -89,10 +110,13 @@ function App() {
             <hr className="footerLine" />
             <div className="footerInfo">
               <div className="text grey" id="footerDetail">
-              This website is created as part of Hlsolutions program. The materials contained on this website are provided for general
-information only and do not constitute any form of advice. HLS assumes no responsibility for the accuracy of any particular statement and
-accepts no liability for any loss or damage which may arise from reliance on the information contained on this site.
-
+                This website is created as part of Hlsolutions program. The
+                materials contained on this website are provided for general
+                information only and do not constitute any form of advice. HLS
+                assumes no responsibility for the accuracy of any particular
+                statement and accepts no liability for any loss or damage which
+                may arise from reliance on the information contained on this
+                site.
               </div>
               <div className="text">Copyright 2021 HLS</div>
             </div>
